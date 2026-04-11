c20743a28e5ab0814e0eb9291c86afeca155081e
Attackcoin <Attackcoin@users.noreply.github.com>
2026-04-11 11:17:51 +0700

    chore(client): add USE_EMULATOR switch to wire Firebase emulators
    Build with --dart-define=USE_EMULATOR=true to route Auth (9099) and
    Firestore (8080) to the local emulator. Web/desktop/iOS use 127.0.0.1;
    Android emulator uses 10.0.2.2.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,14 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
 
+const bool useFirebaseEmulator = bool.fromEnvironment('USE_EMULATOR');
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  if (useFirebaseEmulator) {
+    final host = (!kIsWeb && defaultTargetPlatform == TargetPlatform.android)
+        ? '10.0.2.2'
+        : '127.0.0.1';
+    await FirebaseAuth.instance.useAuthEmulator(host, 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator(host, 8080);
+  }
   runApp(const ProviderScope(child: DaziApp()));
 }
